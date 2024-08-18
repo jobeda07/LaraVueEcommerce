d898f3ec7aeda4fba3b610c6690d240ac4449135
jobeda <jobedakhatun710@gmail.com>
2024-08-19 01:11:54 +0600

    model add

---
+++ database/migrations/2024_08_18_173304_create_user_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->text('address');
+            $table->string('city', 255);
+            $table->string('state', 45)->nullable();
+            $table->string('zipcode', 45);
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+};
