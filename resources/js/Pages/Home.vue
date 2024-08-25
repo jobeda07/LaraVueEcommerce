ca52300b305b3cde410240e6ce628d1a1a648e96
jobeda07 <jobedakhatun710@gmail.com>
2024-08-25 12:43:30 +0600

    home page done

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,7 +4,7 @@
         <Hero/>
         <!-- Hero End -->
         <!-- product start -->
-        <ProductList/>
+        <ProductList :products="products" />
         <!-- product end -->
         <!-- footer -->
         <Footer/>
@@ -12,12 +12,8 @@
 </template>
 
 <script setup>
-import {
-    onMounted
-} from "vue";
-import {
-    initFlowbite
-} from "flowbite";
+import { onMounted} from "vue";
+import {initFlowbite} from "flowbite";
 onMounted(() => {
     initFlowbite();
 });
@@ -25,6 +21,9 @@ import ProductList from "./Frontend/Pages/ProductList.vue";
 import Hero from "./Frontend/Pages/Hero.vue";
 import Layout from "./Frontend/Layout.vue";
 
+defineProps({
+    products: Array
+})
 </script>
 
 <style lang="">
